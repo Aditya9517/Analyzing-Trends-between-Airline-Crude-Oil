b67734f8c6c49c8f7abb3cea12e16140e9f51876
Aditya Jayanti <33398626+Aditya9517@users.noreply.github.com>
2023-10-24 03:54:46 +0530

    Add files via upload

--- parallel_flows.py
+++ parallel_flows.py
@@ -1,5 +1,4 @@
 from prefect import task, flow #-Line 1
-from prefect_dask.task_runners import DaskTaskRunner #-Line 2
 
 #print square of a number
 @task #-Line 3
@@ -9,7 +8,7 @@ def print_squares(element):#-Line4
 
 #The flow that is going to call the tasks
 #This function will be the entry point for our script
-@flow(task_runner=DaskTaskRunner())#-Line 5
+@flow()#-Line 5
 def my_flow(elements):#-Line 6
     for element in elements:
         print_squares.submit(element)#-Line 7
